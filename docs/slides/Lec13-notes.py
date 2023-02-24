76c466c56079ccfa5c88bc60bc8f0a17b84cfbef
Colin Rundel <rundel@gmail.com>
2023-02-24 10:57:03 -0500

    Make update

--- docs/slides/Lec13-notes.py
+++ docs/slides/Lec13-notes.py
@@ -44,10 +44,17 @@ def mle_gamma(θ):
   if θ[0] <= 0 or θ[1] <= 0:
     return 1e16
   else:
-    return -np.sum(np.log(gamma.pdf(x, a=θ[0], scale=θ[1])))
+    return -np.sum(gamma.logpdf(x, a=θ[0], scale=θ[1]))
+
+mle_gamma([1,1])
 
 optimize.minimize(
   mle_gamma, x0=[1,1], method="bfgs"
+)
+
+optimize.minimize(
+  mle_gamma, x0=[1,1], method="l-bfgs-b",
+  bounds=[(1e-8,1e8),(1e-8,1e8)]
 )
 
 gamma.fit(x, floc=0)
